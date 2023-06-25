d03ada68711ed91fb4aa69bdbc4df5008c28c816
minh160496 <114970677+minh160496@users.noreply.github.com>
2023-06-26 00:11:59 +0700

    Update Hero.tsx

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -29,7 +29,7 @@ export const Hero = () => {
   const top = useBreakpointValue({ base: "90%", md: "50%" });
   const side = useBreakpointValue({ base: "30%", md: "10px" });
 
-  const banners = ["public/banner.png"];
+  const banners = ["/banner.png"];
 
   return (
     <Box
